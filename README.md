11c8ac1c32710b22e9124ef7f14ba2d597743611
Masashi <t15519ms@gmail.com>
2021-06-29 22:53:13 +0900

    [add]activeadminの導入

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 RubyとVueで作成したサンプルです
 railsでAPIを作成し、Vueで画面を作っています
 
+以下の記事を参考にさせて頂きました
+Ruby on Rails, Vue.jsで始めるモダンWebアプリケーション入門
+```
+https://qiita.com/tatsurou313/items/4f18c0d4d231e2fb55f4
+```
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ActiveAdmin.routes(self)
+  
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :employees, only: [:index, :show]
+    end
+  end
 end
